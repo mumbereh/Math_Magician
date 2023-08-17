a62dd71fdb8068137e9eaa8b0683d385831810ef
mumbereh <siirasonh@gmail.com>
2023-08-17 20:15:04 +0300

    created Math magicians UI

---
+++ src/componets/Demo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import classes from './Demo.module.css';
+
+export default function Demo() {
+  return (
+    <div className={classes.inner}>Demo</div>
+  );
+}
---
+++ src/componets/Home.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import classes from './Home.module.css';
+import Button from './Button';
+
+export default function Home() {
+  const buttons = [
+    'AC', '+/-', '%', '+',  '7', '8','9', 'X', '4', '5', '6', '-', '1', '2','3','+' , '0', '.','=',
+  ];
+
+  return (
+    <div className={classes.home}>
+      <div className={classes.inner}>
+        <div className={classes.result}>
+          <div className={classes.resbox} />
+        </div>
+        <div className={classes.btns}>
+          {buttons.map((value) => <Button value={value} key={value} />)}
+        </div>
+      </div>
+    </div>
+  );
+}
