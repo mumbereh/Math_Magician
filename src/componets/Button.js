a245f4ecb7d260b936570cf63a22fbc9a7577505
mumbereh <siirasonh@gmail.com>
2023-08-18 16:32:54 +0300

    Add eventlisteners to the calculator buttons

--- src/componets/Button.js
+++ src/componets/Button.js
@@ -3,20 +3,23 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import classes from './Button.module.css';
 
-function Button({ value }) {
+function Button({ value, onClick }) {
   let buttonClassName = classes.calcBtns;
 
   if (value === '0' || value === '0') {
     buttonClassName += ` ${classes.res}`;
-  } else if (value === 'X' || value === '-' || value === '=' || value === '+') {
+  } else if (value === 'x' || value === '-' || value === '=' || value === '+') {
     buttonClassName += ` ${classes.side}`;
   }
 
-  return <div className={buttonClassName}>{value}</div>;
+  return (
+    <button type="button" className={buttonClassName} onClick={onClick}>{value}</button>
+  );
 }
 
-Button.propTypes = {
-  value: PropTypes.string.isRequired,
-};
+Button.propTypes = { value: PropTypes.string.isRequired };
+Button.defaultValue = { value: 0 };
+
+Button.propTypes = { onClick: PropTypes.func.isRequired };
 
 export default Button;
--- src/componets/Calculator.js
+++ src/componets/Calculator.js
@@ -7,36 +7,45 @@ class Calculator extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      object: {
-        total: 0,
-        next: 0,
-        operation: '',
-      },
+      obj: {},
     };
+    this.btn = '';
   }
 
-  handleClick(btnLabel) {
-    const obj = this.state;
-    this.setState({ object: calculate(obj, btnLabel) });
-    console.log(this.state);
+  handleClick = (btn) => {
+    const { obj } = this.state;
+    this.setState({ obj: calculate(obj, btn) });
+    this.btn = btn;
+  };
+
+  showData = (obj) => {
+    if (obj.next === 'AC') {
+      return 0;
+    }
+
+    if (this.btn === '=') {
+      return obj.total;
+    }
+    return obj.next;
   }
 
   render() {
     const buttons = [
-      'AC', '+/-', '%', '+', '7', '8', '9', 'X', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',
+      'AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',
     ];
+    const { obj } = this.state;
     return (
       <div className={classes.home}>
         <div className={classes.inner}>
           <div className={classes.result}>
-            <div className={classes.resbox} />
+            <input className={classes.resbox} value={this.showData(obj)} />
           </div>
           <div className={classes.btns}>
             {buttons.map((value) => (
               <Button
                 value={value}
                 key={value}
-                // onClick={this.handleClick(value)}
+                onClick={() => this.handleClick(value)}
               />
             ))}
           </div>
