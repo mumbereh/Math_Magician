d3790ed6a92afb4627e6291b3ff394906e4c49ca
mumbereh <siirasonh@gmail.com>
2023-08-18 20:31:35 +0300

    add functionality to the AC button to clear

--- src/componets/Calculator.js
+++ src/componets/Calculator.js
@@ -1,58 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classes from './Home.module.css';
 import Button from './Button';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      obj: {},
-    };
-    this.btn = '';
-  }
+function Calculator() {
+  const [obj, setObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const buttons = [
+    'AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',
+  ];
 
-  handleClick = (btn) => {
-    const { obj } = this.state;
-    this.setState({ obj: calculate(obj, btn) });
-    this.btn = btn;
+  const handleClick = (btn) => {
+    setObj(calculate(obj, btn));
   };
-
-  showData = (obj) => {
-    if (obj.next === 'AC') {
-      return 0;
-    }
-
-    if (this.btn === '=') {
-      return obj.total;
-    }
-    return obj.next;
-  }
-
-  render() {
-    const buttons = [
-      'AC', '+/-', '%', '÷', '7', '8', '9', 'x', '4', '5', '6', '-', '1', '2', '3', '+', '0', '.', '=',
-    ];
-    const { obj } = this.state;
-    return (
-      <div className={classes.home}>
-        <div className={classes.inner}>
-          <div className={classes.result}>
-            <input className={classes.resbox} value={this.showData(obj)} />
-          </div>
-          <div className={classes.btns}>
-            {buttons.map((value) => (
-              <Button
-                value={value}
-                key={value}
-                onClick={() => this.handleClick(value)}
-              />
-            ))}
-          </div>
+  return (
+    <div className={classes.home}>
+      <div className={classes.inner}>
+        <div className={classes.result}>
+          <input className={classes.resbox} value={obj.next || obj.total || '0'} />
+        </div>
+        <div className={classes.btns}>
+          {buttons.map((value) => (
+            <Button
+              value={value}
+              key={value}
+              onClick={() => handleClick(value)}
+            />
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default Calculator;
