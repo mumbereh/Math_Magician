b1deab9f8c7bb51a9b9e97eb8d45717e44e83532
mumbereh <siirasonh@gmail.com>
2023-08-19 12:51:06 +0300

    add api functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './componets/Calculator';
+import Quote from './componets/Quote';
 
 function App() {
   return (
     <>
       <Calculator />
+      <Quote />
     </>
   );
 }
