95d5815ef5a34d16926b340551a4446f8d1a5720
mumbereh <siirasonh@gmail.com>
2023-08-24 17:14:40 +0300

    add home.js, Navbar.js, Home.css & Navbar.css

--- src/index.js
+++ src/index.js
@@ -1,11 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
+import * as ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from 'react-router-dom';
 import App from './App';
+import reportWebVitals from './components/reportWebVitals';
+import Quote from './components/Quote';
+import Calculator from './components/Calculator';
+import Home from './components/Home';
+import PageError from './components/error';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <PageError />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: 'Home/',
+        element: <Home />,
+      },
+      {
+        path: 'Calculator/',
+        element: <Calculator />,
+      },
+      {
+        path: 'Quotes/',
+        element: <Quote />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
+
+reportWebVitals();
